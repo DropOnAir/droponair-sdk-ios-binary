05879e5bcf30a1e09633b63de8014fdf01a59755
Amir <amirmoradabadi@gmail.com>
2026-05-23 21:24:06 +0200

    release: 0.18.0
    Update binaryTarget to 0.18.0 XCFramework.
    Checksum: 24a6c0785da4ddb732f5815ccf291065b27c308108de3c78dd3a35f9860ab79c

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DropOnAirSDKBinary",
-            url: "https://github.com/DropOnAir/droponair-sdk-ios-binary/releases/download/0.17.0/DropOnAirSDK.xcframework.zip",
-            checksum: "671c3e22db3d43a1bd5a797cc27ef5787e017d38f23f2c9c530a00be32790398"
+            url: "https://github.com/DropOnAir/droponair-sdk-ios-binary/releases/download/0.18.0/DropOnAirSDK.xcframework.zip",
+            checksum: "24a6c0785da4ddb732f5815ccf291065b27c308108de3c78dd3a35f9860ab79c"
         ),
         .target(
             name: "DropOnAirSDKWrapper",
